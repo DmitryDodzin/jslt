39dbea761ec5218c02ac9e7aae2858f4c9b0b65c
Dmitry Dodzin <d.dodzin@gmail.com>
2024-01-20 14:19:28 +0200

    Update

--- node/check-bin.js
+++ node/check-bin.js
@@ -9,6 +9,11 @@ const runtimeLibc = () =>
 try {
   require(`jslt-node-${process.platform}${runtimeLibc()}-${process.arch}/jslt.node`);
 } catch (e) {
+  if (e.code !== 'MODULE_NOT_FOUND') {
+    console.error(e);
+    process.exit(1);
+  }
+
   const fs = require("fs");
 
   if (!fs.existsSync(`./bin/index.node`)) {
