7e6b14f3c26a58ebcd803927f9aea4dbbffb0600
Dmitry Dodzin <d.dodzin@gmail.com>
2024-02-09 14:40:08 +0200

    Whitespaces

--- node/index.d.ts
+++ node/index.d.ts
@@ -1,23 +1,23 @@
 declare type NativeSchema = symbol;
 
 declare namespace Jslt {
-	export class Jslt {
-        constructor(schema: string);
+  export class Jslt {
+    constructor(schema: string);
 
-		transform<T = any, R = any>(value?: T): R;
+    transform<T = any, R = any>(value?: T): R;
 
-        transformStr(value: string): string;
+    transformStr(value: string): string;
 
-        transformParse<R = any>(value: string): R;
-	}
+    transformParse<R = any>(value: string): R;
+  }
 
-	export function compile(schema: string): NativeSchema;
+  export function compile(schema: string): NativeSchema;
 
-	export function transform<T = any, R = any>(schema: string | NativeSchema, value?: T): R;
+  export function transform<T = any, R = any>(schema: string | NativeSchema, value?: T): R;
 
-	export function transformStr(schema: string | NativeSchema, value: string): string;
+  export function transformStr(schema: string | NativeSchema, value: string): string;
 
-	export function transformParse<R = any>(schema: string | NativeSchema, value: string): R;
+  export function transformParse<R = any>(schema: string | NativeSchema, value: string): R;
 }
 
 export = Jslt;
